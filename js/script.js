24be7348f5df3c51347c388b3fbe3d8a982bab85
Peter Forberg <peterforberg@gmail.com>
2021-02-08 05:08:57 -0600

    assignment 3

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function becomeChef() {
+    var a = document.getElementById("hatted");
+        a.style.display = "block";
+    var b = document.getElementById("hatless");
+        b.style.display = "none";
+    var c = document.getElementById("amchef");
+        c.style.display = "block";
+    var d = document.getElementById("notchef");
+        d.style.display = "none";
+}
+
+function unbecomeChef() {
+    var a = document.getElementById("hatted");
+        a.style.display = "none";
+    var b = document.getElementById("hatless");
+        b.style.display = "block";
+    var c = document.getElementById("amchef");
+        c.style.display = "none";
+    var d = document.getElementById("notchef");
+        d.style.display = "block";
+}
+
+function checkTime() {
+    let selectedElement = document.getElementById("minute");
+    console.log(selectedElement);
+    const now = new Date();
+    selectedElement.innerText = now.getMinutes();
+}
